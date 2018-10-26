454162423f87eb0cfdb382b7b1c95541b5fd839b
Jeetendra Choudhary <mailme@jeetendra.xyz>
2018-10-26 09:18:44 +0530

    updated static server basic

--- 1 - Static Server/StaticServer.js
+++ 1 - Static Server/StaticServer.js
@@ -1,56 +1,57 @@
-'use strict';
+const http = require('http')
+  , url	= require('url')  
+  , fs = require('fs')
+  , path = require('path')
 
-const http	= 	require('http');
-const url	= 	require('url');
-const fs	=	require('fs');
-const path	=	require('path');
+// This are the mimes used to serve and decode types of request resources.
+let mimes =	{
+  '.htm':'text/html',
+  '.css':'text/css',
+  '.js' :'text/javascript',
+  '.gif':'image/gif',
+  '.jpg':'image/jpeg',
+  '.png':'image/png'
+}
 
-let mimes 	=	{
-				'.htm':'text/html',
-				'.css':'text/css',
-				'.js' :'text/javascript',
-				'.gif':'image/gif',
-				'.jpg':'image/jpeg',
-				'.png':'image/png'
-		}
-
-
-function webserver(req,res){
-	// if the route requested is '/', then load 'index.htm' or
-	// else load the requested file(S)
-	let baseURI = url.parse(req.url);
-	let filepath = __dirname+ (baseURI.pathname === '/' ? '/index.htm': baseURI.pathname); 
+/* The actual web server logic the core of any node application
+ * The most foundation level example this can get advanced as much as required.
+ */
+let webserver = ((req,res) => {
+  // if the route requested is '/', then load 'index.htm' or
+  // else load the requested file(S)
+  let baseURI = url.parse(req.url)
+  let filepath = __dirname+ (baseURI.pathname === '/' ? '/index.htm': baseURI.pathname) 
 	
-	// Check if the requested file is accessible or not
-	fs.access(filepath,fs.F_OK, error=>{
-		if(!error){
-			//Read and serve the file
-			fs.readFile(filepath,(error,content)=>{
-				if(!error){
-					// Resolve the content type requested
-					let contentType = mimes[path.extname(filepath)];
-					// serve the file from the buffer
-					console.log('Serving content: '+filepath);
-					res.writeHead(200,{'Content-Type':contentType});
-					res.end(content,'utf-8');
-				}else{
-					// serve a 500 error
-					res.writeHead(500);
-					res.end('The server could not read the file requested.');
-				}
-			});
-		}else{
-			// Serve a 404 content not found
-			res.writeHead(404);
-			res.end('Content not found!');
-		}
-	});
-	console.log(filepath);
+  // Check if the requested file is accessible or not
+  fs.access(filepath,fs.F_OK, error => {
+    if(!error){ 
+      //Read and serve the file
+      fs.readFile(filepath,(error,content) => {
+        if(!error){
+          // Resolve the content type requested
+          let contentType = mimes[path.extname(filepath)]
+          // serve the file from the buffer
+          console.log('Serving content: '+filepath)
+          res.writeHead(200,{'Content-Type':contentType})
+          res.end(content,'utf-8')
+        }else{
+          // serve a 500 error
+          res.writeHead(500)
+          res.end('The server could not read the file requested.')
+        }
+      })
+    }else{
+      // Serve a 404 content not found
+      res.writeHead(404)
+      res.end('Content not found!')
+    }
+  })
+  console.log(filepath)
 
 
-};
+})
 
-http.createServer(webserver).listen(3000,()=>{
-	console.log('WebServer running at port 30000');
+http.createServer(webserver).listen( 3000, () => {
+  console.log('WebServer running at port 30000')
 })
 
--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 #NodeJs Code snippets and reusable apps.
 
-## Basic Router
- - An example of simple router script, to handle get and post requrest with query parameter and POST Header.
-
-## Static Server
+## [Static Server](./1\ -\ Static\ Server/StaticServer.js)
  - An example of static node server to serve static websites. Using NodeJs.
+ - Pre-Requisite -
+  - Basic understanding of `http` module.
+  - Basic understanding of `url` module.
+  - Basic understanding of `fs` module.
+  - Basic understanding of `path` module.
 
 ## Static Server Pro
- - An Advanced version of `Static Server` using Promise instead of nested call backs & Stream to manage input/output buffer.
+  - An Advanced version of `Static Server` using Promise instead of nested call backs & Stream to manage input/output buffer.
+
+## Basic Router
+ - An example of simple router script, to handle get and post request with query parameter and POST Header.
+ - The foundation for understanding express middleware and its usage
 
 ## GitHub
  - Node js version of api hosting example of `Github APIs` consumption.
